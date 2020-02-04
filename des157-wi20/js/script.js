6b5a299a502f31f18571f239ab41651e2e34e7af
mson97@github.com <mhson@ucdavis.edu>
2020-02-04 11:54:34 -0800

    changes to processing

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,18 +1,28 @@
-// source: William Mead
 function setup() {
     var myCanvas = createCanvas(800, 250);
-    myCanvas.parent(mySketch);
-    frameRate(8);
-    noStroke();
-    background(255,255,255);
+    myCanvas.parent('mySketch');
+    frameRate(15);
+    background('#ffffff');
 }
+
+/*
+var colors = ['#FE938C', '#E6B89C', '#EAD2AC', '#EAD2AC', '#9CAFB7', '#4281A4']
+
+function randomInt(min, max){
+    return (Math.random() * (max - min + 1) + min);
+} */
   
 function draw() {
-    if(mouseIsPressed) {
-        fill(0);
+    if(mouseIsPressed && (mouseButton == LEFT)) {
+      noStroke();
+      fill(random(255), random(255), random(255), random(255));
     } else {
-        fill(random(255), random(255), random(255), random(255));
+      fill('#ffffff');
+      stroke(random(255), random(255), random(255), random(255));
     }
-    var circSize = random(100);
-    ellipse(mouseX, mouseY, circSize, circSize);
-}
+    var triangleSize = random(50);
+    var x2 = mouseX + triangleSize/2;
+    var y2 = mouseY - triangleSize;
+    var x3 = mouseX + triangleSize;
+    triangle(mouseX, mouseY, x2, y2, x3, mouseY);
+  }
