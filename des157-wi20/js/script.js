f27aeca54c6d342f4f1580ee2e5e47c54c7ecaf7
mson97 <mson97@users.noreply.github.com>
2020-02-06 01:15:22 -0800

    tweaked coloring to match page

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,26 +1,19 @@
 function setup() {
     var myCanvas = createCanvas(800, 250);
     myCanvas.parent('mySketch');
-    frameRate(15);
+    frameRate(6);
     background('#ffffff');
 }
-
-/*
-var colors = ['#FE938C', '#E6B89C', '#EAD2AC', '#EAD2AC', '#9CAFB7', '#4281A4']
-
-function randomInt(min, max){
-    return (Math.random() * (max - min + 1) + min);
-} */
   
 function draw() {
     if(mouseIsPressed && (mouseButton == LEFT)) {
-      noStroke();
-      fill(random(255), random(255), random(255), random(255));
-    } else {
       fill('#ffffff');
-      stroke(random(255), random(255), random(255), random(255));
+      stroke(random(100,255), random(0,50), random(0,100), random(255)); 
+    } else {
+      noStroke();
+      fill(random(100,255),random(0,50),random(0,100), random(255));
     }
-    var triangleSize = random(50);
+    var triangleSize = random(100);
     var x2 = mouseX + triangleSize/2;
     var y2 = mouseY - triangleSize;
     var x3 = mouseX + triangleSize;
