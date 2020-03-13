b77525ef69096a069d9bf15c77dfd49047a3fe24
mson97 <mson97@users.noreply.github.com>
2020-03-13 06:02:47 -0700

    commented javascript

--- des157-wi20/final/script.js
+++ des157-wi20/final/script.js
@@ -3,11 +3,13 @@
 (function(){
     const nextBtnImg = '<img src="images/next.png" alt="next" class="next oscillate zoom">';
 
+    /* All buttons/links that need smooth scroll when clicked */
     const navLinks = document.querySelectorAll('nav ul li a');
     const nextBtns = document.querySelectorAll('.icon-btn a');
     const rectBtn = document.querySelectorAll('.rect-btn');
     const paragLinks = document.querySelectorAll('.parag a');
 
+    /* Add event handler to smooth scroll on slick for all buttons and links*/
     navLinks.forEach(function(eachLink) {
         eachLink.addEventListener('click', smoothScroll);
     });
@@ -25,26 +27,27 @@
     });
 
 
-    
+    /* smoothScroll based on example by Bill Mead - DES157 Winter 2020 */
     function smoothScroll(event) {
-        
+
         event.preventDefault();
-     
-        
+
         let targetID = event.target.getAttribute('href');
-       
+
+        // the ID of the link lies in the outer element
         if (targetID == null) {
             targetID = event.target.parentElement.getAttribute('href');
         }
-        
+
         const targetAnchor = document.querySelector(targetID);
-        
+
+        // define the original top of the page
         const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top);
         window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-        //console.log(originalTop);
     }
 
 
+    /* On page load, set up positions of each section to update selected element in nav */
     window.addEventListener('load', function() {
         const posts = document.querySelectorAll('section');
         const postTops = [];
@@ -53,15 +56,16 @@
         let counter = 0;
         let prevCounter = 0;
 
+        // save each section's top position into posts array
         posts.forEach(function(post) {
             postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
         });
-        
-        console.log(postTops);
 
+        /* Keep track of current page top as user scrolls */
+        /* If they scroll past a top of a section, update the counter */
         window.addEventListener('scroll', function() {
+            // The first section has a top margin of 250 px
             pageTop = window.pageYOffset + 250;
-            // console.log(pageTop);
 
             if (pageTop > postTops[counter + 1]) {
                 counter++;
@@ -76,6 +80,8 @@
                 //this.console.log(`last post: ${counter}`);
             }
 
+            /* User has scrolled to a different section */
+            /* Update the selected section in the nav */
             if (counter != prevCounter) {
                 navLinks.forEach(function(eachLink) {
                     eachLink.removeAttribute('class');
@@ -87,7 +93,8 @@
         });
 
     });
-    
+
+    /* For now, if the window is resized, we will move the page back to the top */
     let resizeId;
     window.addEventListener('resize', function() {
         this.clearTimeout(resizeId);
@@ -97,6 +104,9 @@
         this.window.location.reload(true);
     }, 500);
 
+    /* Reveal portions of the timeline image in the fifth section */
+    /* Sections will be shown sequentially after mouse is hovered over image */
+    /* Display final text and next button once timeline image is all shown */
     document.querySelector('#fifth #timeline img').addEventListener("mouseover", function(event) {
         const timeline = document.querySelector('#timeline');
         setTimeout(function() {
@@ -119,18 +129,22 @@
 
     const circles = document.querySelectorAll('#sixth li');
 
+    /* Add event handlers for each circle image in sixth section */
     circles.forEach(function(eachCircle){
         eachCircle.addEventListener('mouseover', showMetric);
     });
 
+    /* Set opacity of metric (numbers) to 1 if user hovers over the circle */
     function showMetric(event) {
             const circle_alt = event.target.getAttribute('alt');
             const circle_num = circle_alt[circle_alt.length - 1];
+            // The circle num matches the metric num in the id's
             const metric = document.getElementById('metric' + circle_num);
             metric.classList.remove('hidden');
             metric.classList.add('fadeIn');
 
             const hidden = document.querySelectorAll('#sixth .hidden');
+            // Once all metrics are shown, display the subtext and next button
             if (hidden.length === 0) {
                 const sixthText = document.querySelector('#sixth .subtext');
                 sixthText.classList.remove('subtext');
@@ -142,17 +156,24 @@
             }
     };
 
+    /* Add onClick event handlers for the cloud images in seventh section */
     document.getElementById('cloud1').addEventListener("click", function(event) {
         const seventhHeader = document.querySelector('#seventh h1');
+        // hide header
         seventhHeader.classList.add('fadeOut');
         const seventhText = document.getElementById('seventh-text');
         seventhText.classList.add('fadeIn');
+        // replace text
         seventhText.innerHTML = "Shall we see how personal time has changed?<a href='#eighth'>Sure.</a>";
 
+        // add smooth scroll since button is added after initial page load
         const seventhNext = seventhText.querySelector('a');
         seventhNext.addEventListener("click", smoothScroll);
     });
-    
+
+    /* Add onClick event handlers for cloud images in eighth section */
+    /* Swap images and text with next chronological element */
+    /* Lastly, reveal next button */
     document.getElementById('cloud2').addEventListener("click", function(event){
         event.target.setAttribute('src', 'images/cloud3.png');
         event.target.parentNode.setAttribute('id', 'cloud3');
@@ -160,7 +181,7 @@
         let eighthSubText = document.getElementById('eighth-subtext');
         eighthText.innerHTML = "5 years ago";
         eighthSubText.innerHTML = "A news report in 2014 stated that Apple CEO Steve Jobs did not allow his kids to use the iPad.";
-        
+
 
         document.getElementById('cloud3').addEventListener("click", function(event){
             eighthText = document.getElementById('eighth-text');
@@ -176,7 +197,7 @@
                 eighthText = document.getElementById('eighth-text');
                 eighthSubText.style.display = "none";
                 eighthText.classList.add('fadeIn')
-                
+
                 eighthText.innerHTML = "Unlike the other areas of life, personal time has experienced drastic change.<br>It's no question that screen time is responsible for this decrease.";
                 const eighthNext = document.querySelector('#eighth .icon-btn a');
                 eighthNext.classList.add('fadeIn');
@@ -184,7 +205,5 @@
             });
         });
     });
-    
-
 
 })();
