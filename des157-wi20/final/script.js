41b6c8c7dd15e1f608f7a5ae60d12537b5ef31e6
mson97 <mson97@users.noreply.github.com>
2020-03-13 05:04:47 -0700

    minor edits

--- des157-wi20/final/script.js
+++ des157-wi20/final/script.js
@@ -1,15 +1,13 @@
 'use strict';
 
 (function(){
-    const nextBtnImg = '<img src="images/next.png" alt="next" class="next">';
+    const nextBtnImg = '<img src="images/next.png" alt="next" class="next oscillate zoom">';
 
-   // const startBtn = document.querySelector('#start');
     const navLinks = document.querySelectorAll('nav ul li a');
     const nextBtns = document.querySelectorAll('.icon-btn a');
-
     const rectBtn = document.querySelectorAll('.rect-btn');
+    const paragLinks = document.querySelectorAll('.parag a');
 
-    //startBtn.addEventListener('click', smoothScroll);
     navLinks.forEach(function(eachLink) {
         eachLink.addEventListener('click', smoothScroll);
     });
@@ -22,11 +20,17 @@
         eachRectBtn.addEventListener('click', smoothScroll);
     });
 
+    paragLinks.forEach(function(eachPLink) {
+        eachPLink.addEventListener('click', smoothScroll);
+    });
 
+
+    
     function smoothScroll(event) {
         
         event.preventDefault();
      
+        
         let targetID = event.target.getAttribute('href');
        
         if (targetID == null) {
@@ -40,6 +44,7 @@
         //console.log(originalTop);
     }
 
+
     window.addEventListener('load', function() {
         const posts = document.querySelectorAll('section');
         const postTops = [];
@@ -52,7 +57,7 @@
             postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
         });
         
-        //console.log(postTops);
+        console.log(postTops);
 
         window.addEventListener('scroll', function() {
             pageTop = window.pageYOffset + 250;
@@ -60,15 +65,15 @@
 
             if (pageTop > postTops[counter + 1]) {
                 counter++;
-                this.console.log(`scrolling down ${counter}`);
+                //this.console.log(`scrolling down ${counter}`);
             } else if (counter > 0 && pageTop < postTops[counter]) {
                 counter--;
                 lastPost = posts.length - 1;
-                this.console.log(`scrolling up ${counter}`);
+                //this.console.log(`scrolling up ${counter}`);
             } else if (pageTop > postTops[lastPost]) {
                 counter = lastPost;
                 lastPost++;
-                this.console.log(`last post: ${counter}`);
+                //this.console.log(`last post: ${counter}`);
             }
 
             if (counter != prevCounter) {
@@ -92,25 +97,30 @@
         this.window.location.reload(true);
     }, 500);
 
-    document.querySelector('#fifth #timeline img').addEventListener("click", function(event) {
+    document.querySelector('#fifth #timeline img').addEventListener("mouseover", function(event) {
         const timeline = document.querySelector('#timeline');
-        if (timeline.classList.contains('timeline1')) {
-            timeline.classList.remove('timeline1');
-            timeline.classList.add('timeline2');
-        } else if (timeline.classList.contains('timeline2')) {
-            timeline.classList.remove('timeline2');
-            timeline.classList.add('timeline3');
-            
-            document.querySelector('#fifth-text').innerHTML = "we also work about the same amount, about 8.5 hours.<br>our 9-5 work day hasn’t seen much changes";
-            document.querySelector('#fifth .icon-btn a').innerHTML = nextBtnImg;
-            
-        }
+        setTimeout(function() {
+            if (timeline.classList.contains('timeline1')) {
+                timeline.classList.remove('timeline1');
+                timeline.classList.add('timeline2');
+                setTimeout(function() {
+                    timeline.classList.remove('timeline2');
+                    timeline.classList.add('timeline3');
+                    const fifthText = document.querySelector('#fifth-text');
+                    fifthText.classList.add('fadeIn');
+                    fifthText.innerHTML = "We work about the same amount, about 8.5 to 9 hours per day.<br>The 9 to 5 work day has not seen much change.";
+                    const fifthNext = document.querySelector('#fifth .icon-btn a');
+                    fifthNext.classList.add('fadeIn');
+                    fifthNext.innerHTML = nextBtnImg;
+                }, 1000);
+            }
+        }, 1000);
     });
 
     const circles = document.querySelectorAll('#sixth li');
 
     circles.forEach(function(eachCircle){
-        eachCircle.addEventListener('click', showMetric);
+        eachCircle.addEventListener('mouseover', showMetric);
     });
 
     function showMetric(event) {
@@ -118,43 +128,63 @@
             const circle_num = circle_alt[circle_alt.length - 1];
             const metric = document.getElementById('metric' + circle_num);
             metric.classList.remove('hidden');
+            metric.classList.add('fadeIn');
 
             const hidden = document.querySelectorAll('#sixth .hidden');
             if (hidden.length === 0) {
                 const sixthText = document.querySelector('#sixth .subtext');
                 sixthText.classList.remove('subtext');
-                sixthText.innerHTML = 'so far, not much has changed in sleep, work, and survival activities';
+                sixthText.classList.add('fadeIn')
+                sixthText.innerHTML = 'In the last decade, we have experienced constant hours of sleep, work, and survival activities.';
                 const sixthNext = document.querySelector('#sixth .icon-btn a');
+                sixthNext.parentElement.classList.add('fadeIn');
                 sixthNext.innerHTML = nextBtnImg;
             }
     };
 
     document.getElementById('cloud1').addEventListener("click", function(event) {
+        const seventhHeader = document.querySelector('#seventh h1');
+        seventhHeader.classList.add('fadeOut');
         const seventhText = document.getElementById('seventh-text');
-        seventhText.innerHTML = "let’s see how personal time has <a href='#eighth'>changed over the years</a>";
+        seventhText.classList.add('fadeIn');
+        seventhText.innerHTML = "Shall we see how personal time has changed?<a href='#eighth'>Sure.</a>";
+
+        const seventhNext = seventhText.querySelector('a');
+        seventhNext.addEventListener("click", smoothScroll);
     });
     
     document.getElementById('cloud2').addEventListener("click", function(event){
         event.target.setAttribute('src', 'images/cloud3.png');
         event.target.parentNode.setAttribute('id', 'cloud3');
         let eighthText = document.getElementById('eighth-text');
+        let eighthSubText = document.getElementById('eighth-subtext');
         eighthText.innerHTML = "5 years ago";
+        eighthSubText.innerHTML = "A news report in 2014 stated that Apple CEO Steve Jobs did not allow his kids to use the iPad.";
+        
 
         document.getElementById('cloud3').addEventListener("click", function(event){
+            eighthText = document.getElementById('eighth-text');
+            eighthSubText = document.getElementById('eighth-subtext');
             event.target.setAttribute('src', 'images/cloud4.png');
             event.target.parentNode.setAttribute('id', 'cloud4');
-            eighthText = document.getElementById('eighth-text');
             eighthText.innerHTML = "today";
+            eighthSubText.innerHTML = "In 2019, the worldwide average for internet usage per day exceeded 6 hours.";
 
             document.getElementById('cloud4').addEventListener("click", function(event){
                 event.target.setAttribute('src', 'images/cloud5.png');
                 event.target.parentNode.setAttribute('id', 'cloud5');
                 eighthText = document.getElementById('eighth-text');
-                eighthText.innerHTML = "unlike the other parts of our lives, our personal time has drastically decreased.<br>screen time now takes up most of this precious, limited reserve.";
+                eighthSubText.style.display = "none";
+                eighthText.classList.add('fadeIn')
+                
+                eighthText.innerHTML = "Unlike the other areas of life, personal time has experienced drastic change.<br>It's no question that screen time is responsible for this decrease.";
                 const eighthNext = document.querySelector('#eighth .icon-btn a');
+                eighthNext.classList.add('fadeIn');
                 eighthNext.innerHTML = nextBtnImg;
             });
         });
     });
     
+
+
 })();
