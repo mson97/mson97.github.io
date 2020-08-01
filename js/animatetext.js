0a033c1f35a10086f81ddefed226b4822049fd42
Minhee Son <minhee.son@wdc.com>
2020-08-01 04:36:04 -0700

    revamp about page

--- js/animatetext.js
+++ js/animatetext.js
@@ -6,32 +6,32 @@
     textWrapper.innerHTML = textWrapper.textContent.replace(/[^ ]/g, "<span class='letter'>$&</span>");
     
 
-    anime.timeline({loop: false, duration:5000})
+    anime.timeline({loop: false, duration:9000})
     .add({
         targets: '.ml11 .line',
         scaleY: [0,1],
         opacity: [0.5,1],
         easing: "easeOutExpo",
-        duration: 1000
+        duration: 500
     })
     .add({
         targets: '.ml11 .line',
         translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
         easing: "easeOutExpo",
-        duration: 700,
-        delay: 300
+        duration: 1000,
+        delay: 500
     }).add({
         targets: '.ml11 .letter',
         opacity: [0,1],
         easing: "easeOutExpo",
-        duration: 900,
+        duration: 1500,
         offset: '-=775',
-        delay: (el, i) => 34 * (i+1)
+        delay: (el, i) => 100 * (i+1)
     }).add({
         targets: '.ml11',
         opacity: 1,
-        duration: 1000,
+        duration: 2000,
         easing: "easeOutExpo",
-        delay: 1200
+        delay: 2000
     })
 })();
